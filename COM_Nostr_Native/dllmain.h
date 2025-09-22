db65ac3ed4ba786478cd7582754c69a2d54e57a8
小村 豪 <gomurin0428@gmail.com>
2025-09-23 08:34:10 +0900

    Native追加

---
+++ COM_Nostr_Native/dllmain.h
@@ -0,0 +1,10 @@
+﻿// dllmain.h : モジュール クラスの宣言です。
+
+class CCOMNostrNativeModule : public ATL::CAtlDllModuleT< CCOMNostrNativeModule >
+{
+public :
+	DECLARE_LIBID(LIBID_COMNostrNativeLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_COMNOSTRNATIVE, "{73a9e0cc-1c6c-4e6f-bbec-ce22a88b4087}")
+};
+
+extern class CCOMNostrNativeModule _AtlModule;
